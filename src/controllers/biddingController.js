bb08df43e01062b745641e2519e1423cd14ae75d
thanhdd318 <thanhdd@ints.vn>
2023-04-28 01:44:54 +0700

    update api bidding

--- src/controllers/biddingController.js
+++ src/controllers/biddingController.js
@@ -1,11 +1,14 @@
+const Bidding = require("../models/bidding.model");
 const { HTTPStatusCode } = require("../constants");
 
 const biddingController = {
   updateBiddingList: async (req, res) => {
     try {
       const dataBidding = req.body.bidding;
-
-      return res.status(HTTPStatusCode.OK).json("hih");
+      const data = dataBidding.map((b) => new Bidding(b));
+      const saved = data.map((d) => d.save());
+      const result = await Promise.all(saved);
+      return res.status(HTTPStatusCode.OK).json(result);
     } catch (err) {
       console.log(err);
     }
