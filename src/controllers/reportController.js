a5c4e637dc1cd0a5f61cf1eb3da6c8fc61daa09d
dthanhff <dthanhff318@gmail.com>
2023-06-21 16:34:26 +0700

    update api report

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,32 +1,57 @@
 const moment = require("moment");
 const Plan = require("../models/plan.model");
 const Store = require("../models/store.model");
+const Group = require("../models/group.model");
 const Department = require("../models/department.model");
 const { HTTPStatusCode } = require("../constants");
 
 const reportController = {
   getReportExportToDepartment: async (req, res) => {
     try {
-      const { timeRange, department, typePlan } = req.body;
+      const { timeRange, department, typePlan, group } = req.body;
+      for (const g of group) {
+        const checkAvailableGroup = await Group.findById(g);
+        if (!checkAvailableGroup) {
+          return res.status(HTTPStatusCode.NOT_FOUND).json("Group not found");
+        }
+      }
+      for (const d of department) {
+        const checkAvailableDepartment = await Department.findById(d);
+        if (!checkAvailableDepartment) {
+          return res
+            .status(HTTPStatusCode.NOT_FOUND)
+            .json("Department not found");
+        }
+      }
       const startDate = moment(timeRange[0], "DD MM YY");
       const endDate = moment(timeRange[1], "DD MM YY");
-      const objQuery = { department };
-      if (typePlan === 1 || typePlan === 2) {
-        objQuery.typePlan = typePlan;
-      }
-      const listTicket = await Plan.find(objQuery);
-      const departmentDetail = await Department.findById(department);
+      const listTicket = await Plan.find({
+        department: { $in: department },
+        typePlan: { $in: typePlan },
+      });
       const historyExport = listTicket
         .filter((e) => {
           const timeSend = moment(e.createdTime, "DD MMM YYYY");
           return timeSend.isBetween(startDate, endDate);
         })
         .reduce((acc, cur) => [...acc, ...cur.planList], []);
-      const listSupply = await Store.find({}).populate({
-        path: "company",
-        model: "Supplier",
-        select: "name",
-      });
+
+      const listSupply = await Store.find({})
+        .populate({
+          path: "company",
+          model: "Supplier",
+          select: "name",
+        })
+        .populate({
+          path: "group",
+          model: "Group",
+          select: "name",
+        })
+        .populate({
+          path: "unit",
+          model: "Unit",
+          select: "name",
+        });
       const historyExportDetail = historyExport.map((e) => {
         const findSupplyStore = listSupply.find((d) => d._id === e.id);
         if (!findSupplyStore) {
@@ -36,10 +61,11 @@ const reportController = {
           findSupplyStore._doc;
         return { ...rest, id: e.id, quantityExpect: e.quantity };
       });
-      return res.status(HTTPStatusCode.OK).json({
-        historyDetail: historyExportDetail,
-        departrment: departmentDetail,
-      });
+      console.log(historyExportDetail);
+      const historyExportFilterByGroup = historyExportDetail.filter((e) =>
+        group.includes(e.group._id)
+      );
+      return res.status(HTTPStatusCode.OK).json(historyExportFilterByGroup);
     } catch (err) {
       console.log(err);
       return res.status(HTTPStatusCode.INTERNAL_SERVER_ERROR).json(err);
