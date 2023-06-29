1d48a8ace538d5890cfa1667ecbc1c0020bbc3e8
thanhdd318 <thanhdd@ints.vn>
2023-06-29 22:54:08 +0700

    add socket

--- src/server.js
+++ src/server.js
@@ -5,10 +5,17 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const connect = require("./mongoDbConfig/mongoConfig");
 const apiV1 = require("./routes");
+const Server = require("socket.io").Server;
 
 require("dotenv").config();
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +24,21 @@ app.use(bodyParser.json());
 
 app.use("/v1", apiV1);
 connect();
+
+io.on("connection", (socket) => {
+  socket.emit("hi", 123);
+  socket.on("disconnect", () => {
+    // console.log("user disconnected");
+    // disconectTimeout = setTimeout(() => {
+    //   console.log("User disconect the server");
+    // }, 3000);
+  });
+
+  socket.on("connect", () => {
+    console.log("One user reconnect");
+    // clearTimeout(disconectTimeout);
+  });
+});
 
 httpServer.listen(process.env.PORT, process.env.BASE_URL, async () => {
   console.log("Server is running in Port 4000");
